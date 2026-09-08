3d61674d5f367777c7f32945b0889e629661ae84
Revathi B <revathibakkiyaraj49@gmail.com>
2026-09-08 15:38:04 +0530

    Fix product image JSON recursion

--- src/main/java/com/rev/modal/ProductImage.java
+++ src/main/java/com/rev/modal/ProductImage.java
@@ -1,5 +1,6 @@
 package com.rev.modal;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -18,5 +19,6 @@ public class ProductImage {
 
     @ManyToOne
     @JoinColumn(name = "product_id")
+    @JsonIgnore
     private Product product;
 }
